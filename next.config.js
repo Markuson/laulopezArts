77dc00ae5fa901d8046e93ff2f77713a734b28e8
Markuson <el.marcuson@gmail.com>
2021-02-07 19:05:09 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        ADDRESS: "https://api.cloudinary.com/v1_1/laulopezarts/upload",
-        CLOUDNAME:"laulopezarts",
-        UPLOADPRESET:"zj6kl8vv"
-    },
-  };
